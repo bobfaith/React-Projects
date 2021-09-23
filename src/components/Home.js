497ed2de40e653db2858178202c1199d52559ebd
bobfaith <chettri.babin@gmail.com>
2021-09-23 17:48:55 +0530

    "Activated Delete and Update Button"

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,6 @@ const Home = () => {
     
     return (
         <div >
-            <div className="container my-3">
-           <h4>Add a Note</h4>
-           <form className="my-3" action="">
-           <div class="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-           </form>
-           </div>
             <Notes />
         </div>
     )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,70 +2,98 @@ import NoteContext from './noteContext';
 import { useState } from 'react';
 
 const NoteState = (props)=>{
-   const notesInitial = [
-    {
-      "_id": "614479b5319777a0d94a0663",
+  const host = "http://localhost:5000";
+  const notesInitial = []
+  const [notes, setNotes] = useState(notesInitial)
+
+// Get all Notes
+const getNotes = async () => {
+  // API Call 
+  const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE0MDcxMWQxNDUwMDBhZjA5MzYxZjQ0In0sImlhdCI6MTYzMTY5OTU4OX0.0S63kSbL669FlkxEyOiNdGl_hW33LWcWLUwdvp7AyUk"
+    }
+  });
+  const json = await response.json();
+  console.log(json)
+  setNotes(json)
+}
+
+  // Add a Note
+  const addNote = async (title, description, tag) => {
+    //API Call
+    const response = await fetch(`${host}/api/notes/addnote`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE0MDcxMWQxNDUwMDBhZjA5MzYxZjQ0In0sImlhdCI6MTYzMTY5OTU4OX0.0S63kSbL669FlkxEyOiNdGl_hW33LWcWLUwdvp7AyUk" 
+      },
+      body: JSON.stringify({title, description, tag})
+    });
+    
+    const note = {
+      "_id": "61447cdcd6e6e7a447456ff5",
       "user": "6140711d145000af09361f44",
-      "title": "First Title",
-      "description": "Content - Get healthy, get bold in life.",
-      "tag": "Personal",
-      "date": "2021-09-17T11:19:17.862Z",
-      "__v": 0
-    },
-    {
-      "_id": "61447cdcd6e6e7a447471ee3",
-      "user": "6140711d145000af09361f44",
-      "title": "Second Title",
-      "description": "Get healthy, get bold in life.",
-      "tag": "Personal",
+      "title": title,
+      "description": description,
+      "tag": tag,
       "date": "2021-09-17T11:32:44.395Z",
       "__v": 0
-    },
-    {
-        "_id": "61447cdcd6e6e7a447471ee4",
-        "user": "6140711d145000af09361f44",
-        "title": "Third Title",
-        "description": "Get healthy, get bold in life.",
-        "tag": "Personal",
-        "date": "2021-09-17T11:32:44.395Z",
-        "__v": 0
+    };
+    setNotes(notes.concat(note))
+  }
+
+  // Delete a Note
+  const deleteNote = async (id) => {
+    // API Call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE0MDcxMWQxNDUwMDBhZjA5MzYxZjQ0In0sImlhdCI6MTYzMTY5OTU4OX0.0S63kSbL669FlkxEyOiNdGl_hW33LWcWLUwdvp7AyUk"
       },
-      {
-        "_id": "61447cdcd6e6e7a447501ee3",
-        "user": "6140711d145000af09361f44",
-        "title": "Fourth Title",
-        "description": "Get healthy, get bold in life.",
-        "tag": "Personal",
-        "date": "2021-09-17T11:32:44.395Z",
-        "__v": 0
+      
+    });
+    const json = response.json(); 
+    console.log(json)
+
+    console.log("Deleting the note with id: " + id);
+    const newNotes = notes.filter((note) => {return note._id !== id})
+    setNotes(newNotes)
+  }
+
+  // Edit a Note
+  const editNote = async (id, title, description, tag) => {
+    //API Call
+    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE0MDcxMWQxNDUwMDBhZjA5MzYxZjQ0In0sImlhdCI6MTYzMTY5OTU4OX0.0S63kSbL669FlkxEyOiNdGl_hW33LWcWLUwdvp7AyUk"
       },
-      {
-        "_id": "61447cdcd6e6e7a447471e93",
-        "user": "6140711d145000af09361f44",
-        "title": "Fifth Title",
-        "description": "Get healthy, get bold in life.",
-        "tag": "Personal",
-        "date": "2021-09-17T11:32:44.395Z",
-        "__v": 0
-      },
-      {
-        "_id": "61447cdcd6e6e7a447471ff5",
-        "user": "6140711d145000af09361f44",
-        "title": "Sixth Title",
-        "description": "Get healthy, get bold in life.",
-        "tag": "Personal",
-        "date": "2021-09-17T11:32:44.395Z",
-        "__v": 0
+      body: JSON.stringify({title, description, tag})
+    });
+    const json = response.json(); 
+  
+  // Logic to edit in client side
+    for (let index = 0; index < notes.length; index++) {
+      const element = notes[index];
+      if(element._id === id) {
+        element.title = title;
+        element.description = description;
+        element.tag = tag;
       }
-  ]
-  const [notes, setNotes] = useState(notesInitial)
+    }
+  }
     
     return (
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNote, deleteNote, editNote, getNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
-};
+    }
 
 
 export default NoteState;
